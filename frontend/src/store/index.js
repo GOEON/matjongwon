c7a991023a54ca320f1f6a39efcd8c42b143eff6
leesk0502 <leesk0502@gmail.com>
2022-11-20 20:15:47 +0900

    [#13] 목록 작업중

---
+++ frontend/src/store/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  // 글로벌 영역 상태값.
+  state: {
+     gnb: '', // 선택중인 GNB
+  },
+
+  mutations: {
+    /**
+     * GNB를 선택하는 메소드
+     *
+     * @param state 저장소
+     * @param id 선택된 template type
+     */
+     setGnb(state, id) {
+      state.gnb = id;
+    },
+  }
+});

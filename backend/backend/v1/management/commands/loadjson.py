4c54c100cd89ba28dae5b48e6ee8e48d0a463468
Saron Kim <kasrlmoojoo@gmail.com>
2022-12-17 21:22:29 +0900

    refactor code

--- backend/backend/v1/management/commands/loadjson.py
+++ backend/backend/v1/management/commands/loadjson.py
@@ -6,26 +6,30 @@ import os
 class Command(BaseCommand):
     help = 'Load the JSON data and insert it to Place DB'
 
+    DATA_FILE_PATH = os.path.join('../..', 'scrapers', 'data', 'final_contents.json')
+
     def handle(self, *args, **options):
-        json_path = os.path.join('../..', 'scrapers', 'data', 'final_contents.json')
-        with open(json_path) as f:
+        with open(self.DATA_FILE_PATH, 'r') as f:
             data = json.load(f)
             objects = []
-            for i, entry in enumerate(data):
-                obj = Place(
-                    name = entry['name'],
-                    category = entry['category'],
-                    address = entry['address'],
-                    opening_hours = entry['opening_hours'],
-                    score = entry['score'],
-                    menu = entry['menu'],
-                    url = entry['url'],
-                    reviews = entry['reviews'],
-                    thumbnails = entry['thumbnails'],
-                    description = entry['description'],
-                    coordinates = entry['coordinates'],
-                )
-                objects.append(obj)
+            for place in data:
+                objects.append(self._import_place(place))
             Place.objects.bulk_create(objects)
 
             self.stdout.write(self.style.SUCCESS('Successfully added json data'))
+
+    def _import_place(self, place_data):
+        place = Place(
+            name = place_data['name'],
+            category = place_data['category'],
+            address = place_data['address'],
+            opening_hours = place_data['opening_hours'],
+            score = place_data['score'],
+            menu = place_data['menu'],
+            url = place_data['url'],
+            reviews = place_data['reviews'],
+            thumbnails = place_data['thumbnails'],
+            description = place_data['description'],
+            coordinates = place_data['coordinates'],
+        )
+        return place

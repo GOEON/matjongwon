763917b5f8860d9efaac29651d5619f600e7037f
Saron Kim <kasrlmoojoo@gmail.com>
2022-12-18 18:14:32 +0900

    [#19] query 구문에 따른 object만 보내기 구현

--- backend/backend/v1/views.py
+++ backend/backend/v1/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.core import serializers
+from django.db.models import Q
 
 from .models import Place
 import json
@@ -11,31 +12,31 @@ def placeRequest(request):
     page_num = int(request.GET.get('page', 1))
     page_size = int(request.GET.get('size', 10))
 
-    total_count = Place.objects.count()
-    isLastPage = True if ((total_count // page_size) + 1) == page_num else False
+    # Filer the Place objects by address
+    place_objects = Place.objects.filter(Q(address__icontains=query))
 
-    # Meta : Meta data
+    # Caclulate metadata
+    total_count = place_objects.count()
+    isLastPage = ((total_count // page_size) + 1) == page_num
     meta = {
         "total_count": total_count, 
         "count": page_size,
         "is_end": isLastPage
     }
 
-    # Document : Main Content
-    start_num = (page_num - 1) * page_size
-    place_object = Place.objects.all()[start_num : start_num + page_size]
-    document = list(place_object.values())
+    # Retrieve a page of objects from the Place model
+    start_index = (page_num - 1) * page_size
+    end_index = start_index + page_size
+    place_objects = place_objects[start_index : end_index]
+    documents = list(place_objects.values())
 
-    # HttpResponse : creating a dictionary
-    #response = {"meta": meta, "document" : document}
-    response_data = {}
-    response_data['meta'] = meta
-    response_data['document'] = document
+    # Build the response
+    response_data = {
+        'meta': meta,
+        'documents': documents,
+    }
 
-    # return HttpResponse(json.dumps(response_data),
-    #                    content_type="application/json")
-    return JsonResponse(response_data, safe=False, json_dumps_params={'ensure_ascii': False}, status=200)
-#    return HttpResponse(document, content_type="text/json-comment-filtered")
-#    return HttpResponse("Hello, world. You're at the Matjongwon v1 index page. " + 
-#                        query + " " + str(page_num) + " " + str(page_size) + '\n' + 
-#                        json.dumps(meta))
+    return JsonResponse(response_data, 
+        safe=False, 
+        json_dumps_params={'ensure_ascii': False}, 
+        status=200)

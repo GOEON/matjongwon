829d6ba3e11cd220802ece0996e750561b95ea6b
Saron Kim <kasrlmoojoo@gmail.com>
2023-01-29 12:13:06 +0900

    MySQL connected

--- backend/backend/v1/models.py
+++ backend/backend/v1/models.py
@@ -1,21 +1,27 @@
 from django.db import models
-import json
+
 
 class Place(models.Model):
-    name = models.CharField(max_length=200)
-    category = models.JSONField()
-    address = models.CharField(max_length=200)
-    opening_hours = models.JSONField()
-    score = models.JSONField() # Use JsonField for dictionary type
-    # kakao_score = models.DecimalField(max_digits=2, decimal_places=1)
-    # tripadvisor_score = models.DecimalField(max_digits=2, decimal_places=1)
-    # google_score = models.DecimalField(max_digits=2, decimal_places=1)
-    menu = models.CharField(max_length=200)
-    url = models.CharField(max_length=200)
-    reviews = models.JSONField()
-    thumbnails = models.JSONField()
-    description = models.CharField(max_length=300)
-    coordinates = models.JSONField(default=dict)
+    name = models.TextField(blank=True, null=True)
+    category = models.TextField(blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    opening_hours = models.TextField(blank=True, null=True)
+    score_navermap = models.TextField(db_column='score.navermap', blank=True, null=True)
+    score_kakaomap = models.TextField(db_column='score.kakaomap', blank=True, null=True)
+    score_googlemap = models.TextField(db_column='score.googlemap', blank=True, null=True)
+    menu = models.TextField(blank=True, null=True)
+    url = models.TextField(blank=True, null=True)
+    reviews_navermap = models.TextField(db_column='reviews.navermap', blank=True, null=True)
+    reviews_kakaomap = models.TextField(db_column='reviews.kakaomap', blank=True, null=True)
+    reviews_googlemap = models.TextField(db_column='reviews.googlemap', blank=True, null=True)  # Field renamed to remove unsuitable characters.
+    thumbnails = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    coordinates_latitude = models.TextField(db_column='coordinates.latitude', blank=True, null=True)  # Field renamed to remove unsuitable characters.
+    coordinates_longitude = models.TextField(db_column='coordinates.longitude', blank=True, null=True)  # Field renamed to remove unsuitable characters.
 
+    class Meta:
+        managed = False
+        db_table = 'gangnam'
+    
     def __str__(self):
         return self.name

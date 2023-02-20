86812e7f752aa06cdef085b32cfd9891e17d2add
Saron Kim <kasrlmoojoo@gmail.com>
2023-02-20 23:54:13 +0900

    Create db_settings.py

---
+++ backend/backend/db_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '{ip}', # Change "{ip}" to correct address
+        'NAME': 'matjongwon',
+        'USER': 'test',
+        'PASSWORD': '{pwd}', # change "{pwd}" to correct password
+
+    }
+}

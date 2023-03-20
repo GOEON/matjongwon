eb4919c4c7131098016958a3d02738330aae64a4
Goeon Park <gounttt@gmail.com>
2023-03-20 22:06:55 +0900

    kakao login authorization code

--- frontend/src/layout/AppHeader.vue
+++ frontend/src/layout/AppHeader.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
     <b-navbar toggleable="sm" variant="light">
-      <b-navbar-toggle target="sidebar-1" v-b-toggle.sidebar-1></b-navbar-toggle>
+      <b-navbar-toggle
+        target="sidebar-1"
+        v-b-toggle.sidebar-1
+      ></b-navbar-toggle>
 
       <b-navbar-brand href="/">맛종원</b-navbar-brand>
 
@@ -10,42 +13,101 @@
           <b-form-input class="mw-100" placeholder="검색"></b-form-input>
         </b-nav-form>
       </b-navbar-nav>
-      
-      <b-nav pills class="ml-auto">
-        <b-nav-item size="sm" :active="gnb === 'map'"  @click="clickGnb('map')" href="/">지도</b-nav-item>
-        <b-nav-item size="sm" :active="gnb === 'list'" @click="clickGnb('list')" href="/list">목록</b-nav-item>
-        <b-nav-item size="sm" :active="gnb === 'login'" @click="clickGnb('login')" href="/login">로그인</b-nav-item>
-        <b-nav-item size="sm" :active="gnb === 'naver_login'" @click="clickGnb('naver_login')" href="/naver_login">네이버</b-nav-item>
-      </b-nav>
 
-      <!-- <b-navbar-nav class="ml-auto">
-        <b-nav-item href="#">로그인</b-nav-item>
-      </b-navbar-nav> -->
-      
+      <b-nav pills class="ml-auto">
+        <b-nav-item
+          size="sm"
+          :active="gnb === 'map'"
+          @click="clickGnb('map')"
+          href="/"
+          >지도</b-nav-item
+        >
+        <b-nav-item
+          size="sm"
+          :active="gnb === 'list'"
+          @click="clickGnb('list')"
+          href="/list"
+          >목록</b-nav-item
+        >
+        <b-nav-item size="sm" v-b-toggle.sidebar-right>
+          로그인
+          <b-sidebar
+            id="sidebar-right"
+            title="로그인"
+            width="350px"
+            right
+            shadow
+          >
+            <div class="px-3 py-2" style="text-align:center">
+              <h4>로그인을 하면 가고싶은 식당을 저장할 수 있어요</h4>
+              <br />
+              <div>
+                <img
+                  src="../assets/images/kakao_login_button.png"
+                  alt=""
+                  width="320px"
+                  height="80px"
+                  @click="kakaoLogin()"
+                />
+              </div>
+              <br />
+              <div>
+                <img
+                  src="../assets/images/naver_login_button.png"
+                  alt=""
+                  width="320px"
+                  height="80px"
+                />
+              </div>
+            </div>
+          </b-sidebar>
+        </b-nav-item>
+        <b-nav-item size="sm" v-b-toggle.sidebar-right>
+          <!-- <img
+            src="https://cdn-icons-png.flaticon.com/512/6522/6522516.png"
+            alt=""
+            width="18px"
+            height="auto"
+          /> -->
+          <!-- <b-sidebar
+            id="sidebar-right"
+            title="프로필"
+            width="350px"
+            right
+            shadow
+          >
+            <button>로그아웃</button>
+          </b-sidebar> -->
+        </b-nav-item>
+      </b-nav>
       <b-sidebar id="sidebar-1" shadow>
-        <div class="px-3 py-2">
-          
-        </div>
+        <div class="px-3 py-2"></div>
       </b-sidebar>
     </b-navbar>
   </div>
 </template>
 <script>
-
-import {mapMutations, mapState} from 'vuex';
+import { mapMutations, mapState } from "vuex";
 
 export default {
   computed: {
-    ...mapState(['gnb']),
+    ...mapState(["gnb"])
   },
 
   methods: {
-    ...mapMutations(['setGnb']),
+    ...mapMutations(["setGnb"]),
 
     clickGnb(gnb) {
       this.setGnb(gnb);
+    },
+
+    kakaoLogin() {
+      const params = {
+        redirectUri: "http://localhost:8081/oauth/callbacks"
+      };
+      window.Kakao.Auth.authorize(params);
     }
-  },
+  }
 };
 </script>
 <style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,16 +1,16 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from "vue";
+import Router from "vue-router";
 import AppHeader from "../layout/AppHeader";
 import AppFooter from "../layout/AppFooter";
 import MainMap from "../views/MainMap.vue";
 import MainList from "../views/MainList.vue";
 import KakaoLogin from "../views/KakaoLogin.vue";
-import NaverLogin from "../views/NaverLogin.vue";
+import RedirectUri from "../views/RedirectUri.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  mode: 'history', //해쉬값 제거 방식
+  mode: "history", //해쉬값 제거 방식
   routes: [
     {
       path: "/",
@@ -22,31 +22,20 @@ export default new Router({
       }
     },
     {
-      path: '/list',
-      name: 'list',
+      path: "/list",
+      name: "list",
       components: {
         header: AppHeader,
         default: MainList,
         footer: AppFooter
-      },
-    },
-    {
-      path: '/login',
-      name: 'login',
-      components: {
-        header: AppHeader,
-        default: KakaoLogin,
-        footer: AppFooter
       }
     },
     {
-      path: '/naver_login',
-      name: 'naver_login',
+      path: "/oauth/callbacks",
+      name: "redirectUri",
       components: {
-        header: AppHeader,
-        default: NaverLogin,
-        footer: AppFooter
+        default: RedirectUri
       }
     }
   ]
-})
+});
